9c63642e04380073a9ce33b67b1bca14e05925f8
Dmitriy Stupin <dmitriy-stupin@mail.ru>
2022-06-03 15:11:36 +0300

    first commit

---
+++ API/Entitities/Product.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entitities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Article { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+        public string Design { get; set; }
+        public string Structure { get; set; }
+        public bool InStock { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
