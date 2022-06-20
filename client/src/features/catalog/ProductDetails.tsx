2635fca06adcd3c39670876a73b3e4d683fa27fb
Dmitriy Stupin <dmitriy-stupin@mail.ru>
2022-06-20 09:29:47 +0300

    Add Detail Cerd castomization

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,9 @@
-import { Button, Divider, Grid, Typography } from "@mui/material";
+import { Button, Divider, Grid, IconButton, Paper, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function ProductDetails() {
     const {id} = useParams<{id: string}>();
@@ -22,17 +23,53 @@ export default function ProductDetails() {
 
 
     return (
-        
-        <Grid container spacing={6}>
-            <Grid item xs={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{width: '90%'}}/>
-            </Grid>
-            <Grid item xs={3}>
+        <>  <Typography variant="h4">
                 {product.name}
+            </Typography>
+            <Grid container spacing={6}>
+                <Grid item xs={6}>
+                    <img src={product.pictureUrl} alt={product.name} style={{width: '90%'}}/>
+                </Grid>
+                <Grid item xs={3}>
+                    <div>
+                        Состав: {product.productStructure}
+                    </div>
+                    <div>
+                    Размер: {product.size}
+                    </div>
+                    
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper elevation={10} sx={{
+                                        bgcolor: 'background.paper',
+                                        boxShadow: 1,
+                                        borderRadius: 2,
+                                        p: 2,
+                                        minWidth: 340,
+                                        pl: 4,
+                                        }}>
+                    <Typography gutterBottom variant="h4" >
+                        {product.price.toFixed(0)} ₽
+                    </Typography>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <Button size="large" variant='contained' color='primary' sx={{
+                                                                                minWidth: 280,
+                                                                                }}>
+                        Добавить в корзину
+                    </Button>
+                    <Typography gutterBottom  component="div" sx={{
+                                                                 fontSize: 16,
+                                                                 mt: 2,
+                                                                }}>
+                        21-23 июня доставка СДЭК со склада Дмитровское Д1.
+                    </Typography>
+                    </Paper>
+                    
+                </Grid> 
             </Grid>
-            <Grid item xs={3}>
-                <Button variant='contained' color='primary'>Добавить в корзину</Button>
-            </Grid> 
-        </Grid>
+        </>
+        
     )
 }
